9f39c77a2832bbb18ee8fb31fd4be17f6b7c3c08
Arseniy <arseniykukoba16@gmail.com>
2026-06-25 18:24:46 +0300

    Выполнено задание task01

---
+++ task01/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return false;
+
+        // Убираем пробелы и знаки препинания вручную
+        string cleaned = "";
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            {
+                cleaned += c;
+            }
+        }
+
+        if (string.IsNullOrEmpty(cleaned))
+            return false;
+
+        // Проверяем палиндром вручную
+        int left = 0;
+        int right = cleaned.Length - 1;
+
+        while (left < right)
+        {
+            if (cleaned[left] != cleaned[right])
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
